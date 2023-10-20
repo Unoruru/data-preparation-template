bb7b1da55fd3ddcbc0e9e8b52c6b928e40dc8892
Zach Chen <126782368+Unoruru@users.noreply.github.com>
2023-10-20 17:04:14 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='comp0035-data-prep',
+    version='1.0',
+    author='Sarah Sanders',
+    url='https://github.com/nicholsons/comp0035-data-prep',
+    python_requires='>=3.7',
+    packages=find_packages(
+        include=["src"]
+    ),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+    ],
+)
